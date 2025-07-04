85c133b03849f780ad3038ed65b7bbb8c9135ea5
ghn <youlkim20@gmail.com>
2025-07-04 13:36:58 +0900

    factorial function
    try to make factorial function program

---
+++ c++/c++/FileName.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+//함수
+//factoral
+
+int main() {
+
+	int i = 0;//모듈화
+	std::cin >> i;
+	int value = 1;
+
+	//factoral수보다 1번덜 반복하면 된다
+	for (int j = 0; j < i; ++j) {
+		
+		value *= (j + 1);
+
+		
+		
+		
+
+	}
+	std::cout << value << std::endl;
+	return 0;
+	
+
+
+}
